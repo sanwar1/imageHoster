6168096b699bedc589851bbc7e332cda9ba0bc1e
sanwar1 <s.anwar.ahmed1@gmail.com>
2020-05-24 10:19:03 +0530

    Created Comment class

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,56 @@
+package ImageHoster.model;
+
+import java.util.Date;
+
+public class Comment {
+
+    private Integer id;
+
+    private String text;
+
+    private Date createdDate;
+
+    private User user;
+
+    private Image image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
